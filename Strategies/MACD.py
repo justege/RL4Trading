334fd6783a63f1367a528b91c6e7fab439fe0746
e.okur <e.okur@campus.tu-berlin.de>
2022-08-06 11:18:04 +0200

    IQN

---
+++ Strategies/MACD.py
@@ -0,0 +1,51 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas  as pd
+# Request historic pricing data via finance.yahoo.com API
+df = yf.Ticker('BTC-USD').history(period='1y')[['Close', 'Open', 'High', 'Volume', 'Low']]
+# # Calculate MACD values using the pandas_ta library
+# df.ta.macd(close='close', fast=12, slow=26, signal=9, append=True)
+# Get the 26-day EMA of the closing price
+k = df['Close'].ewm(span=12, adjust=False, min_periods=12).mean()
+# Get the 12-day EMA of the closing price
+d = df['Close'].ewm(span=26, adjust=False, min_periods=26).mean()
+# Subtract the 26-day EMA from the 12-Day EMA to get the MACD
+macd = k - d
+# Get the 9-Day EMA of the MACD for the Trigger line
+macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
+# Calculate the difference between the MACD - Trigger for the Convergence/Divergence value
+macd_h = macd - macd_s
+# Add all of our new values for the MACD to the dataframe
+df['macd'] = df.index.map(macd)
+df['macd_h'] = df.index.map(macd_h)
+df['macd_s'] = df.index.map(macd_s)
+# View our data
+pd.set_option("display.max_columns", None)
+print(df)
+
+
+fig = plt.figure(figsize=(10, 7))
+
+# Define position of 1st subplot
+ax = fig.add_subplot(2, 1, 1)
+
+# Set the title and axis labels
+plt.title('Apple Price Chart')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+
+plt.plot(df['Close'], label='Close price')
+
+# Add a legend to the axis
+plt.legend()
+
+# Define position of 2nd subplot
+bx = fig.add_subplot(2, 1, 2)
+
+# Set the title and axis labels
+plt.title('MACD')
+plt.xlabel('Date')
+plt.ylabel('RSI values')
+
+plt.plot(df['macd'], 'm', label='RSI')
+plt.show()
